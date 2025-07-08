318f09a5a7a6982ae4b75c2d8cc955eb6ff345f7
Efe Türkyılmaz <efe.turkyilmaz@stu.khas.edu.tr>
2025-07-08 10:59:54 +0300

    ...

--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+257f146848c8e0418f9863b6d1c490342a90a1ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eddcbdbdfe672b5d7dddc1a1d29a33ec2bfa665")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
