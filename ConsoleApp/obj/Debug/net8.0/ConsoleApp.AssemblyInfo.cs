9f635a07483ff9e75ade5270124523f68804c67c
Efe Türkyılmaz <efe.turkyilmaz@stu.khas.edu.tr>
2025-07-09 10:54:53 +0300

    sonar-project.properties added

--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ce7be12a15b57b8a0bb7af308abdacccb4e7d0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dfaad2a2f20ccb7178819164d1cfba65a3661a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
